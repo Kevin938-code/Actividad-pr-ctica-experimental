7b78ad325c833771a7c636593d95977d440a962e
kevin Ortiz <estudiante.kevin17@gmail.com>
2026-05-29 17:00:18 -0500

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+    <h1>KEVIN MOISES ORTIZ LUNA</h1>
+    <p>Este es el inicio de un aprendizaje nuevo.</p>
+    </>
+  )
+}
+
+export default App
